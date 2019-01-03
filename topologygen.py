5ac486a166f78b688f10b8918a44d83199c67e61
Wataru Noguchi <wnoguchi@pg1x.com>
2019-01-03 11:28:39 +0900

    Fix IP configuration.

--- topologygen.py
+++ topologygen.py
@@ -48,12 +48,12 @@ params = {
                 },
                 {
                     'name': 'GigabitEthernet 0/0',
-                    'address': '100.{{ index }}.2.{{ index }}',
+                    'address': '100.{{ index }}.4.{{ index }}',
                     'mask': mask24,
                 },
                 {
                     'name': 'GigabitEthernet 0/1',
-                    'address': '100.{{ index }}.4.{{ index }}',
+                    'address': '100.{{ index }}.6.{{ index }}',
                     'mask': mask24,
                 },
             ]
