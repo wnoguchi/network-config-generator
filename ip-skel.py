7f0245e0358e6d66dbf0ebab223bc7f34c9526c5
Wataru Noguchi <wnoguchi@pg1x.com>
2019-03-06 07:25:29 +0900

    For IPv6.

--- ip-skel.py
+++ ip-skel.py
@@ -5,14 +5,19 @@ from jinja2 import Environment, FileSystemLoader, Template
 
 env = Environment(loader=FileSystemLoader('templates/'))
 #env = Environment()
-node_template = env.get_template('cisco/router.j2')
+
+# Command Line Argument
+
+#node_template = env.get_template('cisco/router.j2')
+node_template = env.get_template('cisco/router-ipv6.j2')
 output_template = env.get_template('output-templates/dokuwiki.j2')
 
 mask16 = '255.255.0.0'
 mask24 = '255.255.255.0'
 mask32 = '255.255.255.255'
+ipv6_prefix_length = '64'
 
-number_of_routers = 4
+number_of_routers = 5
 number_of_loopback_interfaces = 0
 number_of_gigabit_ethernet_interfaces = 2
 
@@ -37,14 +42,20 @@ for node in range(number_of_routers):
     for loIndex in range(number_of_loopback_interfaces):
         node['interfaces'].append({
             'name': 'Loopback %d' % (loBase + loIndex),
-            'address': '100.10.%d.1' % (loBase + loIndex),
+            'address': '100.10.%d.%d' % (loBase + loIndex, i),
             'mask': mask24,
+            'ipv6_address_link_local': 'fe80::%d' % (i),
+            'ipv6_address': '2001:0:0:0::%d' % (i),
+            'ipv6_length': ipv6_prefix_length,
         })
     for gigIndex in range(number_of_gigabit_ethernet_interfaces):
         node['interfaces'].append({
             'name': 'GigabitEthernet 0/%d' % (gigBase + gigIndex),
             'address': '0.0.0.0',
             'mask': mask24,
+            'ipv6_address_link_local': 'fe80::%d' % (i),
+            'ipv6_address': '2001:0:0:0::%d' % (i),
+            'ipv6_length': ipv6_prefix_length,
         })
     i += 1
 
